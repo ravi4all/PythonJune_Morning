35b8c330b03291ae88949623927f38e805778168
ravi4all <ravikanttyagi.2295@gmail.com>
2017-07-05 10:58:33 +0530

    Add files via upload

--- Applications/Item_CRUD/crud.py
+++ Applications/Item_CRUD/crud.py
@@ -1,14 +1,22 @@
 item_list = []
+items = {}
 
 def add():
     item_name = input("Enter Item Name : ")
     item_price = input("Enter Item Price : ")
     item_desc = input("Enter Item Description : ")
 
-    item_list.append([item_name,item_price,item_desc])
+    items["Name"] = item_name
+    items["Price"] = item_price
+    items["Description"] = item_desc
+
+    # item_list.append([item_name,item_price,item_desc])
     # print(item_list)
     # for item in item_list:
     #     print(item)
+
+    item_list.append(items.copy())
+
     read()
 
 def read():
@@ -18,7 +26,25 @@ def read():
         print(counter,item)
 
 def update():
-    pass
+    to_update = int(input("Enter Product id to update : "))
+    print("You want to update ",item_list[to_update-1])
+    update_choice = input("What do you want to update?? Name or Price : ")
+    if update_choice == "Name":
+        updated_Name = input("Enter Updated Name : ")
+        new_data = item_list[to_update-1]
+        new_data["Name"] = updated_Name
+        # print(new_data)
+        print("Updated List")
+        read()
+    elif update_choice == "Price":
+        updated_Price = input("Enter Updated Price : ")
+        new_data = item_list[to_update-1]
+        new_data["Price"] = updated_Price
+        # print(new_data)
+        print("Updated List")
+        read()
+    else:
+        print("Wrong Choice...")
 
 def delete():
     to_delete = int(input("Enter Item ID : "))
@@ -33,10 +59,23 @@ def sorting():
     pass
 
 def save():
-    pass
+    file = open("Item_List.txt",'w')
+    print("Writing Data....")
+    # file.write(str(item_list))
+    for data in item_list:
+        file.write(str(data)+"\n")
+    print("Successfully written...")
+    # print(str(item_list))
+    file.close()
 
 def load():
-    pass
+    file = open("Item_List.txt",'r')
+    data = file.readlines()
+    for i in data:
+        item_list.append(i)
+    item_list.append(file.readlines())
+    read()
+    file.close()
 
 def errHandler():
     print("Wrong Choice, Try Again....")
