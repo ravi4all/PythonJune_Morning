6d2ebaaeea612311f75e10600206aed3ceeba8d5
ravi4all <ravikanttyagi.2295@gmail.com>
2017-08-15 21:48:56 +0530

    Add files via upload

---
+++ SpeechToText.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+from pygame import mixer
+mixer.init()
+
+# Record Audio
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+ 
+# Speech recognition using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    # print("You said: " + r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY"))
+    print("You said: " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
